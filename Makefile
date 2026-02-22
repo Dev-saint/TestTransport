fb10879c8b17e8b3d602e6d6a0a93ab801c06115
Stanislav Cherkasov <chstas2002@gmail.com>
2026-02-22 17:13:19 +0700

    Проверил работоспособность на Windows 10 и Linux. Внес изменения в main.cpp для корректного отображения русских символов в обоих системах. Удалил Makefile и добавил скрипты сборки для Windows (build.bat) и Linux (build.sh).

--- Makefile
+++
@@ -1,7 +0,0 @@
-BUILD_DIR := build
-
-all: build
-
-build:
-	@mkdir -p $(BUILD_DIR)
-	@cd $(BUILD_DIR) && cmake .. && cmake --build .
--- main.cpp
+++ main.cpp
@@ -1,39 +1,58 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <locale>
+#include <clocale>
+#if defined(_WIN32)
+#include <windows.h>
+#endif
+
 #include "transportFactory.hpp"
 
-int main(int argc, char* argv[]) {
-    
-    if (argc < 2) {
-        std::cout << "Использование: " << argv[0] 
-                << " <тип_транспорта_1> ... <тип_транспорта_n> (цифрой)" << std::endl;
+int main(int argc, char *argv[])
+{
+    std::setlocale(LC_ALL, "");
+    std::locale::global(std::locale());
+
+#if defined(_WIN32)
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+
+    if (argc < 2)
+    {
+        std::cout << u8"Использование: " << argv[0]
+                  << u8" <тип_транспорта_1> ... <тип_транспорта_n> (цифрой)" << std::endl;
         return 1;
     }
 
     std::vector<std::unique_ptr<Transport>> transports;
 
-    for(size_t i = 1; i < argc; ++i){
+    for (size_t i = 1; i < argc; ++i)
+    {
         try
         {
             int value = std::stoi(argv[i]);
             auto transport = TransportFactory::create(static_cast<TransportType>(value));
             transports.push_back(std::move(transport));
         }
-        catch(...)
+        catch (...)
         {
             transports.push_back(nullptr);
         }
-        
     }
 
-    std::cout << "\nИнформация о транспорте:\n";
+    std::cout << u8"\nИнформация о транспорте:\n";
 
-    for (size_t i = 0; i < transports.size(); ++i) {
-        if (transports[i]){
+    for (size_t i = 0; i < transports.size(); ++i)
+    {
+        if (transports[i])
+        {
             transports[i]->print();
-        } else {
-            std::cout << "НЕИЗВЕСТНЫЙ ТИП ТРАНСПОРТА\n";
+        }
+        else
+        {
+            std::cout << u8"НЕИЗВЕСТНЫЙ ТИП ТРАНСПОРТА\n";
         }
 
         std::cout << "\n";
