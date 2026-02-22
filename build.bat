54a58643147c0878ba3f46028bf7602851a150fb
Stanislav Cherkasov <chstas2002@gmail.com>
2026-02-22 20:45:31 +0700

    Добавил проверку на генератор в build.bat, чтобы использовать MinGW при его наличии, а не NMake.

--- build.bat
+++ build.bat
@@ -1,3 +1,8 @@
 @echo off
-cmake -S . -B build
+:: choose generator, override with environment variable if needed
+if "%CMAKE_GENERATOR%"=="" (
+    set "CMAKE_GENERATOR=MinGW Makefiles"
+)
+echo Using CMake generator: %CMAKE_GENERATOR%
+cmake -S . -B build -G "%CMAKE_GENERATOR%"
 cmake --build build
